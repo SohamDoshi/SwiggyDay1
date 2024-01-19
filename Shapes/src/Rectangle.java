ca2a5ed52ed2a6f86294baa0a536a7d58a27d04b
SohamDoshi <sohamdoshi17@gmail.com>
2024-01-19 13:08:47 +0530

    test 2,3,4

--- Shapes/src/Rectangle.java
+++ Shapes/src/Rectangle.java
@@ -1,4 +1,19 @@
 
 public class Rectangle {
 
+	private int length;
+	private int width;
+	
+	public Rectangle(int len, int wid) {
+		this.length = len;
+		this.width = wid;
+	}
+	
+	public int getLength () {
+		return this.length;
+	}
+	
+	public int getWidth () {
+		return this.width;
+	}
 }
--- Shapes/src/ShapesTestcases.java
+++ Shapes/src/ShapesTestcases.java
@@ -1,4 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class ShapesTestcases {
 
+	@Test
+	public void invaildRectangle () {
+		Rectangle rectangle = new Rectangle(10, 10);
+		
+		assertNotEquals(rectangle.getLength(), rectangle.getWidth());
+	}
+	
+	@Test
+	public void vaildRectangle () {
+		Rectangle rectangle = new Rectangle(10, 10);
+		
+		assertEquals(rectangle.getLength(), rectangle.getWidth());
+	}
+	
+	
+	@Test
+	public void calculateArea() {
+		Rectangle rectangle = new Rectangle(15, 10);
+		int area = rectangle.getLength() * rectangle.getWidth();
+		assertEquals(150, area);
+	}
+	
+	@Test
+	public void calculateCircumference() {
+		Rectangle rectangle = new Rectangle(12, 8);
+		int circumference = 2 * (rectangle.getLength() + rectangle.getWidth());
+		assertEquals(40, circumference);
+	}
 }
